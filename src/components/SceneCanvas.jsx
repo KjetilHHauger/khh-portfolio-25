7363a33b79cac0b707bf19e6daceeff8c41a1c7b
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-05-30 19:09:17 +0200

    Clone floor model

--- src/components/SceneCanvas.jsx
+++ src/components/SceneCanvas.jsx
@@ -1,29 +1,35 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stats } from "@react-three/drei";
-import Floor from "./Floor";
+import FloorGrid from "./FloorGrid";
 
 export default function SceneCanvas() {
   return (
     <Canvas
-      camera={{ position: [10, 10, 10], fov: 60 }}
+      camera={{ position: [20, 20, 20], fov: 50 }}
       style={{ width: "100vw", height: "100vh" }}
     >
       <color attach="background" args={["#222"]} />
 
       {/* Lighting */}
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[60, 20, 10]} intensity={1} />
       {/* Helper */}
       <directionalLight position={[5, 5, 5]} intensity={1} castShadow />
       <pointLight position={[2, 2, 2]} intensity={0.5} />
       <axesHelper args={[5]} />
 
       {/* Controls */}
-      <OrbitControls target={[5, 0, 5]} />
+      <OrbitControls
+        target={[0, 0, 0]}
+        enableRotate={false}
+        enableZoom={true}
+        enablePan={true}
+      />
+
       <Stats />
 
       {/* 3D objects */}
-      <Floor
+      <FloorGrid
         url="/models/ModularDungeons/FloorTile.glb"
         position={[0, 0, 0]}
         scale={1}
