b527d422e4b4286d5565633b1c4cdf6323d0dbc8
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-05-31 23:36:21 +0200

    Redid my popup

---
+++ src/components/Interactive/InteractiveModal.jsx
@@ -0,0 +1,24 @@
+import { useGLTF } from "@react-three/drei";
+import { useMemo } from "react";
+import { clone } from "three/examples/jsm/utils/SkeletonUtils";
+
+export default function InteractiveModal({
+  url,
+  position = [0, 0, 0],
+  rotation = [0, 0, 0],
+  scale = 1,
+  onClick,
+}) {
+  const { scene } = useGLTF(url);
+  const model = useMemo(() => clone(scene), [scene]);
+
+  return (
+    <primitive
+      object={model}
+      position={position}
+      rotation={rotation.map((deg) => (deg * Math.PI) / 180)}
+      scale={Array.isArray(scale) ? scale : [scale, scale, scale]}
+      onClick={onClick}
+    />
+  );
+}
