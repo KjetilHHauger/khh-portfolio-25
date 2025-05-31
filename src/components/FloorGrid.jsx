33d50dc9986a35c701846b90aac1248ba2ea4cdd
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-05-31 09:43:46 +0200

    Adjusted floor grid size

--- src/components/FloorGrid.jsx
+++ src/components/FloorGrid.jsx
@@ -1,29 +1,42 @@
 import { useGLTF } from "@react-three/drei";
 import { clone } from "three/examples/jsm/utils/SkeletonUtils";
-import { Box3, Vector3, MeshStandardMaterial } from "three";
+import { MeshStandardMaterial } from "three";
+import { useMemo } from "react";
 
-export default function FloorGrid({ url = "/models/FloorTile.glb" }) {
+export default function FloorGrid({
+  url = "/models/FloorTile.glb",
+  color = "#424242",
+}) {
   const { scene } = useGLTF(url);
 
-  const createTile = (x, z) => {
-    const model = clone(scene);
+  const floors = useMemo(() => {
+    const gridSize = 9;
+    const tileSize = 2;
+    const offset = (gridSize * tileSize) / 2;
+    const instances = [];
 
-    model.position.set(x, 0, z);
-    return <primitive key={`${x}-${z}`} object={model} />;
-  };
+    for (let x = 0; x < gridSize; x++) {
+      for (let z = 0; z < gridSize; z++) {
+        const model = clone(scene);
 
-  const floors = [];
-  const gridSize = 9;
-  const tileSize = 2;
-  const offset = (gridSize * tileSize) / 2;
+        model.traverse((child) => {
+          if (child.isMesh) {
+            child.material = new MeshStandardMaterial({ color });
+            child.castShadow = true;
+            child.receiveShadow = true;
+          }
+        });
 
-  for (let x = 0; x < gridSize; x++) {
-    for (let z = 0; z < gridSize; z++) {
-      const posX = x * tileSize - offset;
-      const posZ = z * tileSize - offset;
-      floors.push(createTile(posX, posZ));
+        const posX = x * tileSize - offset;
+        const posZ = z * tileSize - offset;
+        model.position.set(posX, 0, posZ);
+
+        instances.push(<primitive key={`${x}-${z}`} object={model} />);
+      }
     }
-  }
+
+    return instances;
+  }, [scene, color]);
 
   return <>{floors}</>;
 }
