f63b3e537346427bc7819f3bdfd4863f67231efd
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-06-04 21:37:25 +0200

    Car animation

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,24 @@
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function LoadingScreen({ progress }) {
+  return (
+    <AnimatePresence>
+      <motion.div
+        className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black text-white"
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <img
+          src="/KHH.png"
+          alt="KHH Logo"
+          className="w-[400px] animate-pulse "
+        />
+        <p className="mt-4 text-sm animate-pulse opacity-75">
+          Loading... {Math.floor(progress)}%
+        </p>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
